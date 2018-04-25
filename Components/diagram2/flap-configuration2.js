d91fc5d5dfa8c85384e6efab463f3434bd0c94f1
bbramanti <bbramanti@gmail.com>
2018-04-24 21:15:17 -0500

    updated component specifics

--- Components/diagram2/flap-configuration2.js
+++ Components/diagram2/flap-configuration2.js
@@ -11,10 +11,10 @@ window.FlapConfiguration = {
     'components': {
         'Flap-1':
             {
-                x: 55.6,
-                y: 6.2,
-                w: 21.7,
-                h: 89,
+                x: 10.9,
+                y: 6.6,
+                w: 33.8,
+                h: 84.9,
                 src: 'Flap_1.png',
                 shadow_src: 'Flap_1_shadow.png',
                 border_src: 'Flap_1_stroke.png',
@@ -22,10 +22,10 @@ window.FlapConfiguration = {
             },
         'Flap-2':
             {
-                x: 72,
-                y: 26.4,
-                w: 5.6,
-                h: 10,
+                x: 22.2,
+                y: 34.3,
+                w: 18.4,
+                h: 30.7,
                 src: 'Flap_2.png',
                 shadow_src: 'Flap_2_shadow.png',
                 border_src: 'Flap_2_stroke.png',
@@ -34,10 +34,10 @@ window.FlapConfiguration = {
             },
         'Flap-3':
             {
-                x: 57.4,
-                y: 6.7,
-                w: 20.2,
-                h: 86.7,
+                x: 22.2,
+                y: 24.8,
+                w: 18.4,
+                h: 40.2,
                 src: 'Flap_3.png',
                 shadow_src: 'Flap_3_shadow.png',
                 border_src: 'Flap_3_stroke.png',
@@ -45,10 +45,10 @@ window.FlapConfiguration = {
             },
         'Flap-4':
             {
-                x: 57.6,
-                y: 7.2,
-                w: 19.7,
-                h: 85.2,
+                x: 22.4,
+                y: 7.5,
+                w: 22.8,
+                h: 81.9,
                 src: 'Flap_4.png',
                 shadow_src: 'Flap_4_shadow.png',
                 border_src: 'Flap_4_stroke.png',
@@ -56,10 +56,10 @@ window.FlapConfiguration = {
             },
         'Flap-5':
             {
-                x: 57.3,
-                y: 7.4,
-                w: 20.3,
-                h: 85.5,
+                x: 25.5,
+                y: 29.8,
+                w: 12.1,
+                h: 16.7,
                 src: 'Flap_5.png',
                 shadow_src: 'Flap_5_shadow.png',
                 border_src: 'Flap_5_stroke.png',
@@ -67,14 +67,58 @@ window.FlapConfiguration = {
             },
         'Flap-6':
             {
-                x: 57.3,
-                y: 7.4,
-                w: 20.3,
-                h: 85.6,
+                x: 25.7,
+                y: 29.9,
+                w: 11.6,
+                h: 16.5,
                 src: 'Flap_6.png',
                 shadow_src: 'Flap_6_shadow.png',
                 border_src: 'Flap_6_stroke.png',
                 desc: 'Component 5 description'
-            }
+            },
+            'Flap-7':
+                {
+                    x: 31.3,
+                    y: 24.6,
+                    w: 2.7,
+                    h: 6.3,
+                    src: 'Flap_7.png',
+                    shadow_src: 'Flap_7_shadow.png',
+                    border_src: 'Flap_7_stroke.png',
+                    desc: 'Component 6 description'
+                },
+                'Flap-8':
+                    {
+                        x: 31.3,
+                        y: 22.8,
+                        w: 4.7,
+                        h: 8.1,
+                        src: 'Flap_8.png',
+                        shadow_src: 'Flap_8_shadow.png',
+                        border_src: 'Flap_8_stroke.png',
+                        desc: 'Component 7 description'
+                    },
+                    'Flap-9':
+                        {
+                            x: 27.8,
+                            y: 15.3,
+                            w: 4.9,
+                            h: 7.4,
+                            src: 'Flap_9.png',
+                            shadow_src: 'Flap_9_shadow.png',
+                            border_src: 'Flap_9_stroke.png',
+                            desc: 'Component 8 description'
+                        },
+                        'Flap-10':
+                            {
+                                x: 22.7,
+                                y: 7.5,
+                                w: 22.3,
+                                h: 81.2,
+                                src: 'Flap_10.png',
+                                shadow_src: 'Flap_10_shadow.png',
+                                border_src: 'Flap_10_stroke.png',
+                                desc: 'Component 9 description'
+                            }
     }
 };
