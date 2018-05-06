4cd42f077175b3cae6e078b8dbbbddd819f141c2
soraedo <soraedo@gmail.com>
2018-05-06 17:50:55 -0500

    Diag 2 Config and Diag 1 flap changes
    Changed diag 2 flaps to match background size and fixed config file accordingly. Fixed strokes in diag 1 and made backs consistent with diag 2.

--- diagram2/Components/diagram2/flap-configuration2.js
+++ diagram2/Components/diagram2/flap-configuration2.js
@@ -11,10 +11,10 @@ window.FlapConfiguration = {
     'components': {
         'Flap-1':
             {
-                x: 10.9,
-                y: 6.6,
-                w: 33.8,
-                h: 84.9,
+                x: 20.2,
+                y: 7.4,
+                w: 22.9,
+                h: 85,
                 src: 'Flap_1.png',
                 shadow_src: 'Flap_1_shadow.png',
                 border_src: 'Flap_1_stroke.png',
@@ -22,10 +22,10 @@ window.FlapConfiguration = {
             },
         'Flap-2':
             {
-                x: 22.2,
-                y: 34.3,
-                w: 18.4,
-                h: 30.7,
+                x: 20.8,
+                y: 36.7,
+                w: 18,
+                h: 30,
                 src: 'Flap_2.png',
                 shadow_src: 'Flap_2_shadow.png',
                 border_src: 'Flap_2_stroke.png',
@@ -34,10 +34,10 @@ window.FlapConfiguration = {
             },
         'Flap-3':
             {
-                x: 22.2,
-                y: 24.8,
-                w: 18.4,
-                h: 40.2,
+                x: 20.7,
+                y: 27.5,
+                w: 18,
+                h: 39.3,
                 src: 'Flap_3.png',
                 shadow_src: 'Flap_3_shadow.png',
                 border_src: 'Flap_3_stroke.png',
@@ -45,10 +45,10 @@ window.FlapConfiguration = {
             },
         'Flap-4':
             {
-                x: 22.4,
-                y: 7.5,
-                w: 22.8,
-                h: 81.9,
+                x: 20.3,
+                y: 8.4,
+                w: 24.8,
+                h: 83.9,
                 src: 'Flap_4.png',
                 shadow_src: 'Flap_4_shadow.png',
                 border_src: 'Flap_4_stroke.png',
@@ -56,10 +56,10 @@ window.FlapConfiguration = {
             },
         'Flap-5':
             {
-                x: 25.5,
-                y: 29.8,
-                w: 12.1,
-                h: 16.7,
+                x: 24.5,
+                y: 31.7,
+                w: 12,
+                h: 16.6,
                 src: 'Flap_5.png',
                 shadow_src: 'Flap_5_shadow.png',
                 border_src: 'Flap_5_stroke.png',
@@ -67,10 +67,10 @@ window.FlapConfiguration = {
             },
         'Flap-6':
             {
-                x: 25.7,
-                y: 29.9,
+                x: 24.6,
+                y: 31.9,
                 w: 11.6,
-                h: 16.5,
+                h: 16.3,
                 src: 'Flap_6.png',
                 shadow_src: 'Flap_6_shadow.png',
                 border_src: 'Flap_6_stroke.png',
@@ -78,10 +78,10 @@ window.FlapConfiguration = {
             },
             'Flap-7':
                 {
-                    x: 31.3,
-                    y: 24.6,
+                    x: 30.2,
+                    y: 26.5,
                     w: 2.7,
-                    h: 6.3,
+                    h: 6.1,
                     src: 'Flap_7.png',
                     shadow_src: 'Flap_7_shadow.png',
                     border_src: 'Flap_7_stroke.png',
@@ -89,10 +89,10 @@ window.FlapConfiguration = {
                 },
                 'Flap-8':
                     {
-                        x: 31.3,
-                        y: 22.8,
-                        w: 4.7,
-                        h: 8.1,
+                        x: 30.3,
+                        y: 24.7,
+                        w: 4.6,
+                        h: 7.8,
                         src: 'Flap_8.png',
                         shadow_src: 'Flap_8_shadow.png',
                         border_src: 'Flap_8_stroke.png',
@@ -100,10 +100,10 @@ window.FlapConfiguration = {
                     },
                     'Flap-9':
                         {
-                            x: 27.8,
-                            y: 15.3,
-                            w: 4.9,
-                            h: 7.4,
+                            x: 27.3,
+                            y: 17.3,
+                            w: 4.8,
+                            h: 7.3,
                             src: 'Flap_9.png',
                             shadow_src: 'Flap_9_shadow.png',
                             border_src: 'Flap_9_stroke.png',
@@ -111,10 +111,10 @@ window.FlapConfiguration = {
                         },
                         'Flap-10':
                             {
-                                x: 22.7,
-                                y: 7.5,
-                                w: 22.3,
-                                h: 81.2,
+                                x: 20.5,
+                                y: 9.1,
+                                w: 23.4,
+                                h: 82.7,
                                 src: 'Flap_10.png',
                                 shadow_src: 'Flap_10_shadow.png',
                                 border_src: 'Flap_10_stroke.png',
