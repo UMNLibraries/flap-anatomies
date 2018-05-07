f5f050fccb57e7dab7f15c31f53e52a33b24f7e2
bbramanti <bbramanti@gmail.com>
2018-05-06 23:32:29 -0500

    changed flip direction of 9th flap

--- diagram2/assets/scripts/flapsheet-embed.js
+++ diagram2/assets/scripts/flapsheet-embed.js
@@ -57,6 +57,8 @@ $(document).ready(function(){
     $lastComponent = Mustache.render(componentTemplate, {'idx': idx});
     $wrapper.find('.flip-up').append($lastComponent);
 
+    // change any neccessary flip directions, in this diagram change the 9th flap to down
+    $("#component-9").addClass("flip-dir-down");
 
 	  var index = 0,
 		currentActive = 0,
