158cc53c57075062c4f1c110b19ea4ee2f4cea8e
soraedo <ching041@umn.edu>
2018-10-03 12:34:22 -0500

    Set-up JavaScript file for loading flap components

---
+++ assets/scripts/load-flaps.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	var svgcontainer = $('svg');
+	console.log(FlapConfiguration.components['Flap-1'].src);
+
+// We need to load-in the first element from the configuration.js and append it to the SvG
+
+	// svgcontainer.append()
+});
