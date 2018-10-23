420c22c2eb71023362c2d4941dd33d4c79b8f8a1
simensta <andrea@zooniverse.org>
2018-10-23 10:56:16 -0500

    Update the config to reflect the naming changes

--- diagram2/components/flap-configuration2.js
+++ diagram2/components/flap-configuration2.js
@@ -2,124 +2,124 @@ window.FlapConfiguration = {
     'shadow_suffix': '_shadow',
     'border_suffix': '_stroke',
     'flip-dir': 'left', // 'left', 'right', 'up', 'down'
-    'base-url': 'Components/diagram2',
+    'base-url': 'components',
     'background': {
-        path: 'Background.jpg',
+        path: 'background.jpg',
         width: '1280px',
         height: '1141px'
     },
     'components': {
-        'Flap-1':
+        'flap-1':
             {
                 x: 20.2,
                 y: 7.4,
                 w: 22.9,
                 h: 85,
-                src: 'Flap_1.png',
-                shadow_src: 'Flap_1_shadow.png',
-                border_src: 'Flap_1_stroke.png',
-                desc: 'Flap 1'
+                src: 'flap_1.png',
+                shadow_src: 'flap_1_shadow.png',
+                border_src: 'flap_1_stroke.png',
+                desc: 'flap 1'
             },
-        'Flap-2':
+        'flap-2':
             {
                 x: 20.8,
                 y: 36.7,
                 w: 18,
                 h: 30,
-                src: 'Flap_2.png',
-                shadow_src: 'Flap_2_shadow.png',
-                border_src: 'Flap_2_stroke.png',
-                desc: 'Flap 2',
+                src: 'flap_2.png',
+                shadow_src: 'flap_2_shadow.png',
+                border_src: 'flap_2_stroke.png',
+                desc: 'flap 2',
                 flip_dir: 'down'
             },
-        'Flap-3':
+        'flap-3':
             {
                 x: 20.7,
                 y: 27.5,
                 w: 18,
                 h: 39.3,
-                src: 'Flap_3.png',
-                shadow_src: 'Flap_3_shadow.png',
-                border_src: 'Flap_3_stroke.png',
-                desc: 'Flap 3'
+                src: 'flap_3.png',
+                shadow_src: 'flap_3_shadow.png',
+                border_src: 'flap_3_stroke.png',
+                desc: 'flap 3'
             },
-        'Flap-4':
+        'flap-4':
             {
                 x: 20.3,
                 y: 8.4,
                 w: 24.8,
                 h: 83.9,
-                src: 'Flap_4.png',
-                shadow_src: 'Flap_4_shadow.png',
-                border_src: 'Flap_4_stroke.png',
-                desc: 'Flap 4'
+                src: 'flap_4.png',
+                shadow_src: 'flap_4_shadow.png',
+                border_src: 'flap_4_stroke.png',
+                desc: 'flap 4'
             },
-        'Flap-5':
+        'flap-5':
             {
                 x: 24.5,
                 y: 31.7,
                 w: 12,
                 h: 16.6,
-                src: 'Flap_5.png',
-                shadow_src: 'Flap_5_shadow.png',
-                border_src: 'Flap_5_stroke.png',
-                desc: 'Flap 5'
+                src: 'flap_5.png',
+                shadow_src: 'flap_5_shadow.png',
+                border_src: 'flap_5_stroke.png',
+                desc: 'flap 5'
             },
-        'Flap-6':
+        'flap-6':
             {
                 x: 24.6,
                 y: 31.9,
                 w: 11.6,
                 h: 16.3,
-                src: 'Flap_6.png',
-                shadow_src: 'Flap_6_shadow.png',
-                border_src: 'Flap_6_stroke.png',
-                desc: 'Flap 6'
+                src: 'flap_6.png',
+                shadow_src: 'flap_6_shadow.png',
+                border_src: 'flap_6_stroke.png',
+                desc: 'flap 6'
             },
-            'Flap-7':
-                {
-                    x: 30.2,
-                    y: 26.5,
-                    w: 2.7,
-                    h: 6.1,
-                    src: 'Flap_7.png',
-                    shadow_src: 'Flap_7_shadow.png',
-                    border_src: 'Flap_7_stroke.png',
-                    desc: 'Flap 7'
-                },
-                'Flap-8':
-                    {
-                        x: 30.3,
-                        y: 24.7,
-                        w: 4.6,
-                        h: 7.8,
-                        src: 'Flap_8.png',
-                        shadow_src: 'Flap_8_shadow.png',
-                        border_src: 'Flap_8_stroke.png',
-                        desc: 'Flap 8'
-                    },
-                    'Flap-9':
-                        {
-                            x: 27.3,
-                            y: 17.3,
-                            w: 4.8,
-                            h: 7.3,
-                            src: 'Flap_9.png',
-                            shadow_src: 'Flap_9_shadow.png',
-                            border_src: 'Flap_9_stroke.png',
-                            desc: 'Flap 9',
-                            flip_dir: 'up'
-                        },
-                        'Flap-10':
-                            {
-                                x: 20.5,
-                                y: 9.1,
-                                w: 23.4,
-                                h: 82.7,
-                                src: 'Flap_10.png',
-                                shadow_src: 'Flap_10_shadow.png',
-                                border_src: 'Flap_10_stroke.png',
-                                desc: 'Flap 10'
-                            }
+        'flap-7':
+            {
+                x: 30.2,
+                y: 26.5,
+                w: 2.7,
+                h: 6.1,
+                src: 'flap_7.png',
+                shadow_src: 'flap_7_shadow.png',
+                border_src: 'flap_7_stroke.png',
+                desc: 'flap 7'
+            },
+        'flap-8':
+            {
+                x: 30.3,
+                y: 24.7,
+                w: 4.6,
+                h: 7.8,
+                src: 'flap_8.png',
+                shadow_src: 'flap_8_shadow.png',
+                border_src: 'flap_8_stroke.png',
+                desc: 'flap 8'
+            },
+        'flap-9':
+            {
+                x: 27.3,
+                y: 17.3,
+                w: 4.8,
+                h: 7.3,
+                src: 'flap_9.png',
+                shadow_src: 'flap_9_shadow.png',
+                border_src: 'flap_9_stroke.png',
+                desc: 'flap 9',
+                flip_dir: 'up'
+            },
+        'flap-10':
+            {
+                x: 20.5,
+                y: 9.1,
+                w: 23.4,
+                h: 82.7,
+                src: 'flap_10.png',
+                shadow_src: 'flap_10_shadow.png',
+                border_src: 'flap_10_stroke.png',
+                desc: 'flap 10'
+            }
     }
 };
