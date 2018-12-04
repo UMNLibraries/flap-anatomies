9388a2aeadb726999e9337e8a820a3f0d122199f
simensta <andrea@zooniverse.org>
2018-12-04 14:41:31 -0600

    Add SVG components to the background

--- assets/scripts/load-flaps.js
+++ assets/scripts/load-flaps.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	var svgcontainer = $('svg');
-	console.log(FlapConfiguration.components['Flap-1'].src);
+	console.log(FlapConfiguration.components['flap-1'].src);
 
 // We need to load-in the first element from the configuration.js and append it to the SvG
 
